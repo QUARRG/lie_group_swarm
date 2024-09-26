8825e33a5119b00be75417b833fc924c95b0a4d3
dimitriasilveria <dimitriasilveria.ds@gmail.com>
2024-09-26 17:33:11 -0400

    creating function to send the trajectory

--- crazy_encirclement/crazy_circle.py
+++ crazy_encirclement/crazy_circle.py
@@ -19,7 +19,7 @@ from crazy_encirclement.set_parameter_client import SetParameterClient
 class Circle(Node):
     def __init__(self):
         super().__init__('encirclement')
-        self.declare_parameter('robot_prefix', 'C110')  
+        self.declare_parameter('robot_prefix', 'C103')  
 
         self.robot = self.get_parameter('robot_prefix').value
         #clients
@@ -50,9 +50,9 @@ class Circle(Node):
         self.pose = Pose()
         self.land_flag = False
         self.initial_pose = None
-        
-        self.Tp = 10
-        self.dt = 0.01
+        self.timer_period = 0.01
+        self.Tp = 20
+        self.dt = self.timer_period
         self.t = np.arange(0,self.Tp,self.dt)
         self.N = len(self.t)
 
@@ -70,9 +70,8 @@ class Circle(Node):
         phase = np.arctan2(y,x)
         #center = np.array([np.sign(x)*(np.abs(x)-self.r),np.sign(y)*(np.abs(y)-self.r)])
         self.ra_r = self.r*np.vstack((np.cos(2*np.pi*self.t/self.Tp + phase),np.sin(2*np.pi*self.t/self.Tp+phase),self.hover_height*np.ones(self.N)))
-        self.va_r = self.r*(2*np.pi/self.Tp)*np.vstack((-np.sin(2*np.pi*self.t/self.Tp+phase),np.cos(2*np.pi*self.t/self.Tp+phase),np.zeros(self.N)))
-        self.va_r_dot = -self.r*(2*np.pi/self.Tp)**2*np.vstack((np.cos(2*np.pi*self.t/self.Tp+phase),np.sin(2*np.pi*self.t/self.Tp+phase),np.zeros(self.N)))
-        self.ra_r[2,:] = self.ra_r[2,:]+self.initial_pose.position.z
+        self.trajectory()
+        #self.ra_r[2,:] = self.ra_r[2,:]+self.initial_pose.position.z
         #ra_r = np.vstack((np.cos(2*np.pi*t/Tp),np.sin(2*np.pi*t/Tp),0.5*np.ones(N)))
         self.get_logger().info('Circle node has been started.')
         
@@ -81,8 +80,8 @@ class Circle(Node):
             self.Ca_r[:,:,0] = Ca_r_new
         
         input("Press Enter to takeoff")
-        timer_period = 0.1
-        self.timer = self.create_timer(timer_period, self.timer_callback)
+        
+        self.timer = self.create_timer(self.timer_period, self.timer_callback)
 
     def _landing_callback(self, msg):
         self.land_flag = msg.data
@@ -97,13 +96,15 @@ class Circle(Node):
     def takeoff(self,time_th):
         x= self.initial_pose.position.x
         y= self.initial_pose.position.y
-        t = np.arange(0,1.,0.01)
+        self.t_max = 5
+        t = np.arange(0,self.t_max,self.timer_period)
         r = np.zeros((3,len(t))) 
         r_dot = np.zeros((3,len(t)))
-        r[2,:] = self.hover_height*t #+ self.initial_pose.position.z
-        r_dot[2,:] = self.hover_height*np.ones(len(t))
+        r[2,:] = self.hover_height*(t/self.t_max) #+ self.initial_pose.position.z
+        r_dot[2,1:] = np.diff(r[2,:])/self.timer_period
+        
         msg = FullState()
-        for i in range(len(t)):
+        for i in range(len(t)-1):
             msg.pose.position.x = r[0,i]+x
             msg.pose.position.y = r[1,i]+y
             msg.pose.position.z = r[2,i]
@@ -122,8 +123,17 @@ class Circle(Node):
             msg.twist.angular.z = 0.0
             self.get_logger().info(f"Publishing to {msg.pose.position.x}, {msg.pose.position.y}, {msg.pose.position.z}")
             self.full_state_publisher.publish(msg)
-            time.sleep(0.01)
-        #time.sleep(3)
+            time.sleep(self.timer_period)
+        
+        t_init = time.time()
+        while time.time()-t_init < time_th:
+            msg = FullState()
+            msg.pose.position.x = self.initial_pose.position.x
+            msg.pose.position.y = self.initial_pose.position.y
+            msg.pose.position.z = self.hover_height
+            self.full_state_publisher.publish(msg)
+            self.get_logger().info(f"Publishing to {msg.pose.position.x}, {msg.pose.position.y}, {msg.pose.position.z}")
+            time.sleep(self.timer_period)
         self.has_taken_off = True
 
             #self.reboot()
@@ -147,38 +157,15 @@ class Circle(Node):
                 self.takeoff(5)
 
             elif not self.has_landed and self.pose.position.z > 0.10:#self.ra_r[:,0]:
-                #Wr_r_new, _, _, quat_new, Ca_r_new = generate_reference(self.va_r_dot[:,self.i],self.Ca_r[:,:,self.i],self.va_r[:,self.i],self.dt)
-     
-                # self.msg.pose.position.x = self.ra_r[0,self.i]
-                # self.msg.pose.position.y = self.ra_r[1,self.i]
-                # self.msg.pose.position.z = self.ra_r[2,self.i]
-                # self.msg.acc.x = self.va_r_dot[0,self.i]
-                # self.msg.acc.y = self.va_r_dot[1,self.i]
-                # self.msg.acc.z = self.va_r_dot[2,self.i]
-                # self.msg.pose.orientation.x = quat_new[0]
-                # self.msg.pose.orientation.y = quat_new[1]
-                # self.msg.pose.orientation.z = quat_new[2]
-                # self.msg.pose.orientation.w = quat_new[3]
-                # self.msg.twist.linear.x = self.va_r[0,self.i]
-                # self.msg.twist.linear.y = self.va_r[1,self.i]
-                # self.msg.twist.linear.z = self.va_r[2,self.i]
-                # self.msg.twist.angular.x = Wr_r_new[0]
-                # self.msg.twist.angular.y = Wr_r_new[1]
-                # self.msg.twist.angular.z = Wr_r_new[2]
-                # self.full_state_publisher.publish(self.msg)
-                msg = FullState()
-                msg.pose.position.x = self.initial_pose.position.x
-                msg.pose.position.y = self.initial_pose.position.y
-                msg.pose.position.z = self.hover_height
-                self.full_state_publisher.publish(msg)
-                self.get_logger().info(f"Publishing to {msg.pose.position.x}, {msg.pose.position.y}, {msg.pose.position.z}")
-
-
-                # if self.i == 0:
-                #     time.sleep(5)
-                #     self.set_parameter_client.set_parameter(param_name='all.params.stabilizer.controller',param_value=2)                
+                self.next_point()
+                # msg = FullState()
+                # msg.pose.position.x = self.initial_pose.position.x
+                # msg.pose.position.y = self.initial_pose.position.y
+                # msg.pose.position.z = self.hover_height
+                # self.full_state_publisher.publish(msg)
+            #     # self.get_logger().info(f"Publishing to {msg.pose.position.x}, {msg.pose.position.y}, {msg.pose.position.z}")
                 
-                if self.i < len(self.t)-1:
+                if self.i < len(self.t)-3:
                 #     self.Ca_r[:,:,self.i+1] = Ca_r_new
                     self.i += 1
                 else:
@@ -198,10 +185,10 @@ class Circle(Node):
     
     def landing(self):
         t = np.arange(0,1.,0.01)
-        r = np.zeros((len(t),3)) 
-        r_dot = np.zeros((len(t),3))
-        r[:,2] = self.hover_height*t
-        r_dot[:,2] = -self.hover_height*np.ones(len(t))
+        r = np.zeros((3,len(t))) 
+        r_dot = np.zeros((3,len(t)))
+        r[2,:] = self.hover_height*t
+        r_dot[2,:] = -self.hover_height*np.ones(len(t))
         msg = FullState()
         for i in range(len(t)-1,1,-1):
             msg.pose.position.x = r[0,i]#+self.msg.pose.position.x
@@ -226,7 +213,30 @@ class Circle(Node):
         self.has_landed = True
         self.reboot()
 
+    def next_point(self):
+        Wr_r_new, _, _, quat_new, Ca_r_new = generate_reference(self.va_r_dot[:,self.i],self.Ca_r[:,:,self.i],self.va_r[:,self.i],self.dt)
+        self.Ca_r[:,:,self.i] = Ca_r_new
+        self.msg.pose.position.x = self.ra_r[0,self.i]
+        self.msg.pose.position.y = self.ra_r[1,self.i]
+        self.msg.pose.position.z = self.ra_r[2,self.i]
+        self.msg.acc.x = self.va_r_dot[0,self.i]
+        self.msg.acc.y = self.va_r_dot[1,self.i]
+        self.msg.acc.z = self.va_r_dot[2,self.i]
+        self.msg.pose.orientation.x = quat_new[0]
+        self.msg.pose.orientation.y = quat_new[1]
+        self.msg.pose.orientation.z = quat_new[2]
+        self.msg.pose.orientation.w = quat_new[3]
+        self.msg.twist.linear.x = self.va_r[0,self.i]
+        self.msg.twist.linear.y = self.va_r[1,self.i]
+        self.msg.twist.linear.z = self.va_r[2,self.i]
+        self.msg.twist.angular.x = float(Wr_r_new[0])
+        self.msg.twist.angular.y = float(Wr_r_new[1])
+        self.msg.twist.angular.z = float(Wr_r_new[2])
+        self.full_state_publisher.publish(self.msg)
 
+    def trajectory(self):
+        self.va_r[:,0:-1] = np.diff(self.ra_r[2,:],axis=1)/self.timer_period
+        self.va_r_dot[:,0:-2] = np.diff(self.va_r,axis=1)/self.timer_period
 def main():
     rclpy.init()
     encirclement = Circle()
